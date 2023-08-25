a3d7d181cd8d4402fce9feb835f009c5d3cf9805
Thiago Omoto <thi.omoto@gmail.com>
2023-08-25 10:30:54 -0300

    add no template home

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,7 @@ from news.views import index, news_details, categories_form, news_form
 
 urlpatterns = [
     path("news/<int:id>", news_details, name="news-details-page"),
-    path("categories/", categories_form, name="categories-form"),
     path("news/", news_form, name="news-form"),
+    path("categories/", categories_form, name="categories-form"),
     path("", index, name="home-page"),
 ]
